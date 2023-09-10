a021e415997238d1fb61b40448c3468ba26c2220
david vidal <davidvidal44@gmail.com>
2023-09-10 00:59:19 -0700

    edit update

--- views/show.jsx
+++ views/show.jsx
@@ -2,11 +2,11 @@ const React = require('react')
 const Default = require('./layouts/Default')
 
 function Show ({bread,index}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
+  
       return (
         <Default>
   <h3>{bread.name}</h3>
+
   <p>
     and it
     {
@@ -17,6 +17,8 @@ function Show ({bread,index}) {
     have gluten.
   </p>
   <img src={bread.image} alt={bread.name} />
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
   
 <form action={`/breads/${index}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
